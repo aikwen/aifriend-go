564047bb65eb12d3a63b0dca512bf524737ca8a5
kwen <aikwen@outlook.com>
2026-03-14 13:17:50 +0800

    :recycle: friend 查询

--- internal/store/friend/get_list.go
+++ internal/store/friend/get_list.go
@@ -9,9 +9,11 @@ import (
 func (s *friendStore) GetList(ctx context.Context, userID uint, offset int, limit int) ([]models.Friend, error) {
 	var friends []models.Friend
 
-	err := s.db.WithContext(ctx).InnerJoins("Character").
-		Where("friends.user_id = ?", userID).
-		Order("friends.updated_at desc").
+	err := s.db.WithContext(ctx).
+		Model(&models.Friend{}).
+		Joins("INNER JOIN characters ON characters.id = friends.character_id AND characters.deleted_at IS NULL").
+		Where("friends.user_id = ? AND friends.deleted_at IS NULL", userID).
+		Order("friends.updated_at DESC").
 		Offset(offset).
 		Limit(limit).
 		Preload("Character.Author").
