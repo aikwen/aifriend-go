fc0d34938d833e42ce2e0825ab0d4d409b97d938
kwen <aikwen@outlook.com>
2026-03-18 15:47:20 +0800

    :bug: friend update 1093 错误

--- internal/store/friend/update.go
+++ internal/store/friend/update.go
@@ -8,15 +8,10 @@ import (
 )
 
 func (f *friendStore) UpdateMemoryWithVersion(ctx context.Context, userID uint, friendID uint, oldVersion uint, memory string) error {
-	subQuery := f.db.WithContext(ctx).
-		Model(&models.Friend{}).
-		Select("friends.id").
-		Joins("INNER JOIN characters ON characters.id = friends.character_id AND characters.deleted_at IS NULL").
-		Where("friends.id = ? AND friends.user_id = ? AND friends.deleted_at IS NULL AND friends.version = ?", friendID, userID, oldVersion)
-
 	result := f.db.WithContext(ctx).
 		Model(&models.Friend{}).
-		Where("id IN (?)", subQuery).
+		Joins("INNER JOIN characters ON characters.id = friends.character_id AND characters.deleted_at IS NULL").
+		Where("friends.id = ? AND friends.user_id = ? AND friends.deleted_at IS NULL AND friends.version = ?", friendID, userID, oldVersion).
 		Updates(map[string]any{
 			"memory":  memory,
 			"version": oldVersion + 1,
