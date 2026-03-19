1605a7970d76e580a9349dc63f15339ea840ddfd
kwen <aikwen@outlook.com>
2026-03-20 00:39:33 +0800

    :sparkles:  chat 接入 rag

--- internal/chat/service.go
+++ internal/chat/service.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aikwen/aifriend-go/internal/chat/llm/graph"
 	"github.com/aikwen/aifriend-go/internal/chat/llm/memory"
 	chatmodel "github.com/aikwen/aifriend-go/internal/chat/llm/model"
+	"github.com/aikwen/aifriend-go/internal/chat/llm/rag"
+	"github.com/aikwen/aifriend-go/internal/chat/llm/rag/embeddings"
 	chattools "github.com/aikwen/aifriend-go/internal/chat/llm/tools"
 	"github.com/aikwen/aifriend-go/internal/store"
 	"github.com/aikwen/aifriend-go/internal/store/models"
@@ -15,7 +17,6 @@ import (
 	"github.com/cloudwego/eino/schema"
 )
 
-
 type Service interface {
 	Chat(ctx context.Context, userID uint, friendID uint, message string) (<-chan StreamEvent, error)
 	GetHistory(ctx context.Context, friendID uint, lastMessageID uint, userID uint) ([]models.Message, error)
@@ -23,20 +24,34 @@ type Service interface {
 
 type chatService struct {
 	database  *store.Database
-    graph    *graph.Graph
+	graph     *graph.Graph
 	memorySvc memory.Service
 }
 
 func NewChatService(database *store.Database) (Service, error) {
 	ctx := context.Background()
+	embeddingSvc, err := embeddings.NewEinoSvcFromConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	ragSvc, err := rag.NewServiceFromConfig(embeddingSvc)
+	if err != nil {
+		return nil, err
+	}
 
 	// 创建工具
 	getTimeTool := chattools.NewGetTimeTool()
 	introduceTool := chattools.NewIntroduceAIFriendTool()
+	ragTool, err := chattools.NewSearchKnowledgeBaseTool(ragSvc)
+	if err != nil {
+		return nil, err
+	}
 
 	tools := map[string]einotool.InvokableTool{
-		"get_time":           getTimeTool,
-		"introduce_aifriend": introduceTool,
+		chattools.TimeToolName:                getTimeTool,
+		chattools.IntroduceAIFriendToolName:   introduceTool,
+		chattools.SearchKnowledgeBaseToolName: ragTool,
 	}
 
 	// 提取 ToolInfo
@@ -50,6 +65,11 @@ func NewChatService(database *store.Database) (Service, error) {
 		return nil, fmt.Errorf("get introduce_aifriend tool info failed: %w", err)
 	}
 
+	ragInfo, err := ragTool.Info(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("get search_knowledge_base tool info failed: %w", err)
+	}
+
 	// 创建基础模型
 	cm, err := chatmodel.NewDeepseekChatModelFromConfig(ctx)
 	if err != nil {
@@ -60,7 +80,9 @@ func NewChatService(database *store.Database) (Service, error) {
 	toolModel, err := cm.WithTools([]*schema.ToolInfo{
 		getTimeInfo,
 		introduceInfo,
+		ragInfo,
 	})
+
 	if err != nil {
 		return nil, fmt.Errorf("bind tools to model failed: %w", err)
 	}
@@ -69,8 +91,8 @@ func NewChatService(database *store.Database) (Service, error) {
 	g := graph.NewGraph(toolModel, tools)
 
 	return &chatService{
-		database: database,
-		graph:    g,
+		database:  database,
+		graph:     g,
 		memorySvc: memory.NewService(g, database),
 	}, nil
 }
